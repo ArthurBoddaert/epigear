e0100a4c8f7e11d8b337bce7f308a4ef1c2b2903
ArthurBoddaert <58828825+ArthurBoddaert@users.noreply.github.com>
2020-05-13 21:03:04 +0200

    Comments added and translated

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
-# code source : https://github.com/gastbob40/epigear
-# continué par : Delepoulle Samuel et Boddaert Arthur
-
-
-
+"""
+Source code : https://github.com/gastbob40/epigear
+Continued by : Delepoulle Samuel and Boddaert Arthur
+"""
 from argparse import ArgumentParser
 import discord
 import yaml
@@ -50,53 +49,85 @@ async def on_ready():
     #quit()
 
 # **************************************************************************
-# évènements 
+# events
 # **************************************************************************
 
 @client.event
 async def on_message(message):
+    """Does nothing but allowing usage of events and commands
+
+    Parameters
+    ----------
+    message: Message
+        The message recieved    
+    """
     if message.author == client.user:
         return
     await client.process_commands(message) # nécessaire pour intégrer des des commandes et des évènements
     
 
 # **************************************************************************
-# commandes 
+# commands
 # **************************************************************************
-command_brief="affiche la liste des roles existants"
-command_help="affiche la liste des roles existants\n"
+
+command_brief="Displays a list of the existing roles"
+command_help="Displays a list of the existing roles"
 @client.command(name="rolelist", help=command_help)
 async def rolelist(ctx, *args):
+    """Displays a list of the existing roles
+
+    Parameters
+    ----------
+    ctx: Context
+        The context of the message
+    args: List[str]
+        Every single word following the name of the command
+    """
     embed = discord.Embed(title="--rolelist")
-    texte = ""
+    text = ""
     for role in ctx.guild.roles:
         if role.name != "@everyone":
-            texte += role.name + "\n"
-    embed.description = texte
+            text += role.name + "\n"
+    embed.description = text
     await ctx.send(embed=embed)
 
-command_brief="affiche la liste des utilisateurs du serveur"
-command_help="affiche la liste des utilisateurs du serveur\narguments possibles: nom de rôle, statut, nom d'un salon vocal ou '-o' suivi d'un nom de fichier\n"
-command_help+="  --list : affiche la liste des utilisateurs ayant au moins un rôle\n"
-command_help+="  --list ROLE1 ROLE2 ... : affiche la liste des utilisateurs ayant les rôles spécifiés\n"
-command_help+="  --list STATUT1 STATUT2 ... : affiche la liste des utilisateurs ayant les statuts spécifiés\n"
-command_help+="  --list SALON_VOCAL1 SALON_VOCAL1 ... : affiche la liste des utilisateurs présents dans les salons vocaux spécifiés\n"
-command_help+="  --list -o NOM_DE_FICHIER : crée et upload un fichier contenant les noms des utilisateurs, leurs id et leurs rôles"
+command_brief="Displays a list of users depending on the arguments"
+command_help="Displays a list of users depending on the arguments\nSeveral arguments are allowed:\n"
+command_help+="  - the name of a role, in this case the command only shows the names of the users possessing the specified role\n"
+command_help+="  - a status (online, offline, idle, dnd or invisible), in this case the command only shows the names of the users whose status corresponds to the specified status\n"
+command_help+="  - the name of a voice channel, in this case the command only shows the names of the users currently in the specified channel\n"
+command_help+="  - '-o ' followed by a name, in this case the command will create and upload a file named as specified. This file contains every usernames followed by their IDs and their roles on the server\n"
+command_help+="If the command has none of these arguments, it will simply show the names of every single user of the server\nIn every case, this command ignores bots"
 @client.command(name="list", help=command_help, brief=command_brief) 
 async def list(ctx, *args):
+    """Displays a list of users depending on the arguments
+    Several arguments are allowed:
+    - the name of a role, in this case the command only shows the names of the users possessing the specified role
+    - a status (online, offline, idle, dnd or invisible), in this case the command only shows the names of the users whose status corresponds to the specified status
+    - the name of a voice channel, in this case the command only shows the names of the users currently in the specified channel
+    - '-o ' followed by a name, in this case the command will create and upload a file named as specified. This file contains every usernames followed by their IDs and their roles on the server
+    If the command has none of these arguments, it will simply show the names of every single user of the server
+    In every case, this command ignores bots
+
+    Parameters
+    ----------
+    ctx: Context
+        The context of the message
+    args: List[str]
+        Every single word following the name of the command
+    """
     embed = discord.Embed(title="--list")
-    texte = ""
+    text = ""
     memberList = []
     memberListRole = []
     memberListStatut = []
-    statutList = ["ONLINE", "OFFLINE", "IDLE", "DND", "INVISIBLE"]
-    statutArgsList = []
-    # list sans argument
+    statusList = ["ONLINE", "OFFLINE", "IDLE", "DND", "INVISIBLE"]
+    statusArgList = []
+    # no parameter
     if len(args) == 0:
         for member in ctx.guild.members:
-            if len(member.roles) > 1:
-                if not member.bot:  
-                    memberList.append(member)
+            if not member.bot:  
+                memberList.append(member)
     else:
         if args[0] == "-o" and len(args) == 2:
             memberRoles = ""
@@ -110,16 +141,16 @@ async def list(ctx, *args):
                 file.write(pseudo(member)+":"+str(member.id)+":"+memberRoles+"\n")
             file.close()
             return await ctx.send(file=discord.File("./files/list-o/"+args[1]+".txt", filename=args[1]))
-        # list STATUT
+        # with a status
         for arg in args:
-            if arg.upper() in statutList:
-                statutArgsList.append(arg)
+            if arg.upper() in statusList:
+                statusArgList.append(arg)
                 for member in ctx.guild.members:
                     if check_statut(member, arg):
                         if member not in memberListStatut and not member.bot:
                             memberListStatut.append(member)
 
-        # list ROLE
+        # with a role
         for arg in args:
             for role in ctx.guild.roles:
                 if role.name.upper() == arg.upper():
@@ -128,7 +159,7 @@ async def list(ctx, *args):
                             if member not in memberListRole and not member.bot:
                                 memberListRole.append(member)
 
-        # list SALON_VOCAL
+        # voice channel as a parameter
         for arg in args:
             for voice_channel in ctx.guild.voice_channels:
                 if voice_channel.name.upper() == arg.upper():
@@ -145,31 +176,43 @@ async def list(ctx, *args):
             memberList = memberListRole
         if len(memberListStatut) > 0:
             memberList = memberListStatut
-    texte = str(len(memberList)) + " personnes trouvées" + "\n \n"
+    text = str(len(memberList)) + " personnes trouvées" + "\n \n"
     for memberListItem in memberList:
-        texte += pseudo(memberListItem) + "\n"
-    embed.description = texte;
+        text += pseudo(memberListItem) + "\n"
+    embed.description = text;
     return await ctx.send(embed=embed)
 
 
 @client.command(name="dm")
 async def dmall(ctx, role_arg, *args):
-    # on récupère le role concerné
+    """Sends a specified message to all users who have the specified role
+    This command also allows to send an attachment
+
+    Parameters
+    ----------
+    ctx: Context
+        The context of the message
+    role_arg: str
+        The targeted role
+    args: List[str]
+        Every single word following the name of the command
+    """
+    # specified role
     listRole = []
     for role in ctx.guild.roles:
         if role.name.upper() == role_arg.upper():
             listRole.append(role)
-    # on établit la liste des personnes concernées
+    # list of every targeted user
     destinataires = []
     for member in ctx.guild.members:
         for role in listRole:
             if member not in destinataires and role in member.roles and not member.bot:
                 destinataires.append(member)
-    # on récupère les fichiers attachés
+    # attachments
     attachmentList = []
     for attachment in ctx.message.attachments:
         attachmentList.append(await attachment.to_file())
-    # on envoie le message à chaque personne concernée
+    # send the message to every targeted user
     for destinataire in destinataires:
         await destinataire.send(content=" ".join(args), files=attachmentList)
 
@@ -177,7 +220,23 @@ async def dmall(ctx, role_arg, *args):
 # **************************************************************************
 # **************************************************************************
 
-def pseudo(member): # si le membre s'est renommé sur le serveur on prend son surnom, sinon on prend son pseudo habituel
+def pseudo(member):
+    """Checks if the member has a nickname or not
+
+    Parameters
+    ----------
+    member: Member
+        The targeted discord member
+
+    Returns
+    -------
+    The member's username
+        If the member has no nickname on the server
+    The member's nickname
+        If the member has a nickname on the server
+    Nothing
+        If the parameter isn't a Member
+    """
     if isinstance(member, discord.Member):
         if member.nick is None:
             return member.name
@@ -185,21 +244,37 @@ def pseudo(member): # si le membre s'est renommé sur le serveur on prend son su
             return member.nick
     return
 
-def check_statut(member, statut): # si le statut du membre correspond au statut 'statut' on retourne True
-    if isinstance(member, discord.Member) and isinstance(statut, str):
-        if statut.upper() == "ONLINE":
+def check_statut(member, status):
+    """Checks the member's status
+
+    Parameters
+    ----------
+    member: Member
+        The targeted discord member
+    statut:
+        The targeted status
+
+    Returns
+    -------
+    True
+        If the user's status corresponds to the specified status
+    False
+        If the user's status doesn't corresponds to the specified status or if the parameter isn't a Member
+    """
+    if isinstance(member, discord.Member) and isinstance(status, str):
+        if status.upper() == "ONLINE":
             if member.status == discord.Status.online:
                 return True
-        if statut.upper() == "OFFLINE":
+        if status.upper() == "OFFLINE":
             if member.status == discord.Status.offline:
                 return True
-        if statut.upper() == "IDLE":
+        if status.upper() == "IDLE":
             if member.status == discord.Status.idle:
                 return True
-        if statut.upper() == "DND":
+        if status.upper() == "DND":
             if member.status == discord.Status.dnd:
                 return True
-        if statut.upper() == "INVISIBLE":
+        if status.upper() == "INVISIBLE":
             if member.status == discord.Status.invisible:
                 return True
     return False
